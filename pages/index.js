79e9aeaabff638e5016ddc9ab5e6cf753934078e
Brianna Bias <brianna.bias@gmail.com>
2022-02-20 17:48:46 -0800

    dependancies

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 import ContainerBlock from "../components/ContainerBlock";
 import LatestCode from "../components/LatestCode";
 import Hero from "../components/Hero";
-// import getLatestRepos from "@lib/getLatestRepos";
+import getLatestRepos from "../lib/getLatestRepos";
 import userData from "../constants/data";
 
 export default function Home({ repositories }) {
